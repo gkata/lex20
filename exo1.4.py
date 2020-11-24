15144193cfbbcfb025e8350f7d5b58f98808b9da
Kata Gabor <gabor@lipn.univ-paris13.fr>
2020-11-24 09:05:29 +0100

    Add files via upload

---
+++ exo1.4.py
@@ -0,0 +1,66 @@
+import sys
+import re
+import math
+import nltk
+from nltk.collocations import *
+from nltk.tokenize import word_tokenize
+
+#nltk.download('punkt')
+
+# Ecrivez la fonction 'main' qui lit le fichier donné en argument, le tokenise 
+# avec word_tokenize de nltk et prépare les dictionnaires de mots et de bigrams 
+# (clé: mot/bigram, valeur: fréquence) : 
+# 1) le dictionnaire des mots : wordfreq 
+# 2) le dictionnaire des bigrams : bifreq
+# 3) la fréquence totale des mots : N
+# 4) la fréquence totale des bigrams : B
+
+def main():
+  wordfreq = {}
+  bifreq = {}
+  N = 0
+  B = 0
+  
+ ### your code here ###
+  stop = {}
+  file = open("stopwords.lst", "r")
+  for line in file:
+    stop[line.rstrip()] = 1
+  file.close
+
+  f = open(sys.argv[1], "r")
+  for line in f:
+    words = word_tokenize(line)
+
+    for word in words:
+      if stop.get(word) == 1:
+       continue
+      N += 1
+      if wordfreq.get(word) == None:
+        wordfreq[word] = 1
+      else:
+        wordfreq[word] += 1
+
+    for k in range(1,len(words)):
+      if stop.get(words[k-1]) == 1 or stop.get(words[k]) == 1:
+        continue
+      bigram = words[k-1] + ' ' + words[k]
+      B += 1   
+      if bifreq.get(bigram) == None:
+        bifreq[bigram] = 1
+      else:
+        bifreq[bigram] += 1
+
+  print ('N=' + str(N))
+  print ('B=' + str(B))
+
+  for word in sorted(wordfreq, key=wordfreq.get, reverse=True):
+    print (word + "\t" +  str(wordfreq[word]))
+
+  for word in sorted(bifreq, key=bifreq.get, reverse=True):
+    print (word + "\t" +  str(bifreq[word]))
+
+
+if __name__ == '__main__':
+  main()
+
